e66ec531386ad36c262a0ab69fb704e6a052ebb1
Enzo Casasola <enzocasasola@gmail.com>
2016-09-15 18:52:59 +0200

    Mofication 1 on mcc.c

--- MCC/mcc.c
+++ MCC/mcc.c
@@ -4,9 +4,9 @@
 
 typedef struct dEDGE
 {
-	int vertexID;
-	bool explored;
-	struct EDGE *nextEdge;
+	struct dVERTEX headV;
+	struct dVERTEX tailV;
+	struct dEDGE* nextEdge;
 } tEDGE;
 
 typedef struct dVERTEX
@@ -14,8 +14,81 @@ typedef struct dVERTEX
 	int vertexID;
 	bool explored;
 	int numbOfEdges;
-	struct EDGE* firstEdge;
+	struct dEDGE* firstEdge;
 } tVERTEX;
+
+int addEdge(tVERTEX* graph, int vertexA, int vertexB){
+	tEDGE *e,*e1,*e2;
+
+	e=graph[vertexA-1].firstEdge; //Select first edge connected to the vertexA-1 in the Graph
+	while(e && e->nextEdge){	//Check if this egde exists or not, as well as if there is another edge connected to the original vertexA
+		e=e->nextEdge;	//If it does exist, verify the following edge. The objective is to find the last edge in the array
+	}
+	e1=malloc(sizeof(tEDGE));
+	e1->vertexID=vertexB;
+	e1->nextEdge=NULL;
+	if(e)
+		e->nextEdge=e1;		//Connect the edge vertexA/vertexB to the last part of the array of vertexA
+	else
+		graph[vertexA-1].firstEdge=e1;	//Connect the edge vertexA/vertexB as the first edge of the array of vertexA
+
+	e=graph[vertexB-1].firstEdge; //Select first edge connected to the vertexB-1 in the Graph
+	while(e && e->nextEdge){	//Check if this egde exists or not, as well as if there is another edge connected to the original vertexB
+		e=e->nextEdge;	//If it does exist, verify the following edge. The objective is to find the last edge in the array
+	}
+	e2= malloc(sizeof(tEDGE));
+	e2->vertexID=vertexA;
+	e2->nextEdge=NULL;
+	if(e)
+		e->nextEdge=e2;		//Connect the edge vertexA/vertexB to the last part of the array of vertexA
+	else
+		graph[vertexB-1].firstEdge=e2;	//Connect the edge vertexA/vertexB as the first edge of the array of vertexA
+
+	graph[vertexA-1].numberEdges++;
+	graph[vertexB-1].numberEdges++;
+	return 0;
+
+}
+
+int readGraph(FILE *fp, tVERTEX **graph, int* vertexNumber){
+	int i, j, numbersInLine, arrayNumber[LINE_MAX_SIZE_CHAR];
+	char *ret1, line[LINE_MAX_SIZE_INT];
+
+	while(fgets(line,sizeof(line),fp)!=NULL){
+		getNumbersFromString(line,arrayNumber);
+	}
+
+	while(fgets(line,sizeof(line),fp)!=NULL){
+		(*vertexNumber)++;
+		getNumbersFromString(line,arrayNumber);
+	}
+
+	(*graph)=(tVERTEX* )malloc((*vertexNumber)*sizeof(tVERTEX));
+
+	// ------- Add vertex to the Graph -------
+	rewind(fp);
+	i=0;
+	while(fgets(line,sizeof(line),fp)!=NULL){
+		numbersInLine=getNumbersFromString(line,arrayNumber);
+		(*graph)[i].vertexID=arrayNumber[0];
+		(*graph)[i].enable=TRUE;
+		(*graph)[i].numberEdges=0;
+		(*graph)[i].firstEdge=NULL;
+		i++;
+	}
+	// ------- Add edges to the Graph -------
+	rewind(fp);
+	while(fgets(line,sizeof(line),fp)!=NULL){
+		numbersInLine=getNumbersFromString(line,arrayNumber);
+		for(j=1;j<numbersInLine;j++){
+			//printf("arrayNumber[0]=%d; arrayNumber[%d]=%d\n", arrayNumber[0],j,arrayNumber[j]);
+			if(!checkEdgeExist(*graph,arrayNumber[0],arrayNumber[j]))
+				addEdge(*graph, arrayNumber[0],arrayNumber[j]);
+		}
+	}
+
+	return 0;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -34,6 +107,8 @@ int main(int argc, char const *argv[])
 		printf("ERROR: File doesn't exists\n");
 		return 1;
 	}
+
+	readGraph(fp,&graph,&vertexNumber);
 
 	return 0;
 }
