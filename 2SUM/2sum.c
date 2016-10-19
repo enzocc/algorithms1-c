874155779ebd90a87c22503d673b2e9ac55fd01c
Enzo Casasola <enzocasasola@gmail.com>
2016-10-19 13:59:55 +0200

    added NoDuplicate array

--- 2SUM/2sum.c
+++ 2SUM/2sum.c
@@ -39,11 +39,13 @@ int organize(int** array, int minIndex, int maxIndex){
 }
 
 bool binarySearch(int* array, int arraySize, int lookingFor){
+	if((lookingFor>array[arraySize-1])||(lookingFor<array[0]))
+		return FALSE;
+
 	if(arraySize == 1){
 		if((*array)!=lookingFor)
 			return FALSE;
 		else{
-			//printf("1. Found: %d \n", lookingFor,(*array));
 			return TRUE;
 		}
 	}
@@ -52,7 +54,7 @@ bool binarySearch(int* array, int arraySize, int lookingFor){
 		return TRUE;
 	}
 	else if(array[arraySize/2]<lookingFor){
-		return binarySearch(&array[arraySize/2], arraySize - arraySize/2, lookingFor);
+		return binarySearch(&array[arraySize/2+1], arraySize - arraySize/2-1, lookingFor);//binarySearch(&array[arraySize/2], arraySize - arraySize/2, lookingFor);//
 	}
 	else if(array[arraySize/2]>lookingFor){
 		return binarySearch(array, arraySize/2, lookingFor);
@@ -61,7 +63,7 @@ bool binarySearch(int* array, int arraySize, int lookingFor){
 
 int main(int argc, char const *argv[])
 {
-	int arraySize = 0, *array,count=0,i,j,toLook;
+	int arraySize = 0, *array,*array_noDup,size_noDup, count=0,i,j,toLook;
 	bool exit;
 	FILE *fp;
 	char fileName[50];
@@ -80,22 +82,38 @@ int main(int argc, char const *argv[])
 
 	readArray(fp,&arraySize, &array);
 	organize(&array,0,arraySize-1);
+
+	array_noDup=malloc(arraySize*sizeof(int));
+
+	array_noDup[0]=array[0];
+	size_noDup=1;
+	for(i=1;i<arraySize;i++){
+		if(array[i]!=array[i-1]){
+			array_noDup[size_noDup]=array[i];
+			size_noDup++;
+		}
+		//else
+			//printf("equal!: %d\n", array[i]);
+	}
+	//printArray(size_noDup,array_noDup, "Ordered Array, without duplicates");
+	printf("Size of noDup:%d\n",size_noDup);
+
 	for(j=-10000;j<10001;j++){
 		i=0;
 		exit=FALSE;
 		while(exit==FALSE){
-			toLook=j-array[i];
+			toLook=j-array_noDup[i];
 			//printf("suma=%d => Current element: %d, looking for: %d\n",j,array[i],toLook);
-			if(array[i]==toLook){
-				exit=TRUE;
-			}
-			else if(binarySearch(array,arraySize,toLook)){
+//			if(array_noDup[i]==toLook){
+//				exit=TRUE;
+//			}
+			/*else*/ if(binarySearch(array_noDup,size_noDup,toLook)){
 				count++;
 				exit = TRUE;
 				//printf("Target SUM: %d, Evaluating: %d\n", j, array[i]);
 			}
 			i++;
-			if(i==arraySize)
+			if(i==size_noDup)
 				exit=TRUE;
 		}
 	}
