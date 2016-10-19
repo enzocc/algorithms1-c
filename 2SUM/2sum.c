2110f65229483d0690da92ace5b57eb319ffac95
Enzo Casasola <enzocasasola@gmail.com>
2016-10-19 11:25:14 +0200

    new revision

--- 2SUM/2sum.c
+++ 2SUM/2sum.c
@@ -6,7 +6,7 @@
 	#include "time.h"
 #endif
 
-int selectPivot(int**array, int minIndex, int maxIndex){//Pivot is the median of the right, left and medium elements of the array
+int selectPivot(int**array, int minIndex, int maxIndex){//Pivot is a random number between maxIndex and minIndex
 	return rand()%(maxIndex-minIndex) + minIndex;
 }
 
@@ -42,22 +42,20 @@ bool binarySearch(int* array, int arraySize, int lookingFor){
 	if(arraySize == 1){
 		if((*array)!=lookingFor)
 			return FALSE;
-		else
+		else{
+			//printf("1. Found: %d \n", lookingFor,(*array));
 			return TRUE;
+		}
 	}
 
 	if(array[arraySize/2]==lookingFor){
 		return TRUE;
 	}
-	else{
-		if(array[arraySize/2]<lookingFor){
-			//printf("HOLA 1 - %d\n", arraySize/2);
-			return binarySearch(&array[arraySize/2], arraySize - arraySize/2, lookingFor);
-		}
-		else{
-			//printf("HOLA 2 - %d\n", arraySize - arraySize/2);
-			return binarySearch(array, arraySize/2, lookingFor);
-		}
+	else if(array[arraySize/2]<lookingFor){
+		return binarySearch(&array[arraySize/2], arraySize - arraySize/2, lookingFor);
+	}
+	else if(array[arraySize/2]>lookingFor){
+		return binarySearch(array, arraySize/2, lookingFor);
 	}
 }
 
@@ -81,30 +79,20 @@ int main(int argc, char const *argv[])
 	srand(time(NULL));
 
 	readArray(fp,&arraySize, &array);
-	//printArray(arraySize,array,"Unsorted Array");
 	organize(&array,0,arraySize-1);
-	//printArray(arraySize,array,"Sorted Array: ");
-	for(j=-10000;j<10000;j++){
+	for(j=-10000;j<10001;j++){
 		i=0;
 		exit=FALSE;
 		while(exit==FALSE){
 			toLook=j-array[i];
+			//printf("suma=%d => Current element: %d, looking for: %d\n",j,array[i],toLook);
 			if(array[i]==toLook){
-				if(i>0 && (array[i-1]==toLook)){
-					count++;
-					exit=TRUE;
-				}
-				else if (i<arraySize-1 && (array[i+1]==toLook)){
-					count++;
-					exit=TRUE;	
-				}
-				else
-					exit=TRUE;
+				exit=TRUE;
 			}
 			else if(binarySearch(array,arraySize,toLook)){
 				count++;
 				exit = TRUE;
-				printf("suma=%d => Current element: %d, looking for: %d\n",j,array[i],j-array[i]);
+				//printf("Target SUM: %d, Evaluating: %d\n", j, array[i]);
 			}
 			i++;
 			if(i==arraySize)
